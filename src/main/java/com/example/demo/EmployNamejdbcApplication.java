506bfc8bb214643f496f48f242c01bd46f42ba4b
Kaustubh Anirudha Vaidya <140918319+KaustubhVaidya404@users.noreply.github.com>
2024-05-15 10:13:04 +0530

    initial commit

---
+++ src/main/java/com/example/demo/EmployNamejdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployNamejdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployNamejdbcApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/models/Employer.java
@@ -0,0 +1,12 @@
+package com.example.demo.models;
+
+public class Employer {
+
+	private int id;
+	private String name;
+	private String department;
+	private String post;
+	private int salary;
+	
+	
+}
